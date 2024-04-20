b164e54029737c19e6844485ca6f8b8dd3acdba0
Mojo <moalef@live.com>
2024-04-20 21:25:29 +0330

    Updated models with Ads class. Also added Requirements.txt

--- DivarClone/ads/models.py
+++ DivarClone/ads/models.py
@@ -1,8 +1,9 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.utils import timezone
+
 
 # Create your models here.
-
 
 
 class Category(models.Model):
@@ -12,3 +13,38 @@ class Category(models.Model):
 
     def __str__(self):
         return self.name
+
+
+
+class PublishedManager(models.Manager):
+    def get_queryset(self):
+        return super().get_queryset()\
+                      .filter(status = Ad.Status.PUBLISHED)
+
+
+
+class Ad (models.Model):
+    class Status(models.TextChoices):
+        DRAFT = ('DF' , 'Draft')
+        PUBLISHED = ('PB' , 'Published')
+
+    title = models.CharField(max_length = 250)
+    owner = models.ForeignKey(User , on_delete = models.CASCADE , related_name = 'ads')
+    description = models.TextField()
+    publish = models.DateTimeField(default = timezone.now)
+    created = models.DateTimeField(auto_now_add = True)
+    updated = models.DateTimeField(auto_now = True)
+    status = models.CharField(max_length = 2 , choices = Status.choices , default = Status.DRAFT)
+    
+
+    objects = models.Manager()
+    published = PublishedManager()
+
+    class Meta:
+        ordering = ['-publish' ]
+        indexes = [models.Index(fields= ['-publish'])]
+
+    
+    def __str__(self):
+        return self.title
+    
