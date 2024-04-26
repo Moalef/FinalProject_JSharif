6c577ff5e872b6a1bdc10563ccda68ecb89e8888
Mojo <moalef@live.com>
2024-04-26 15:57:09 +0330

    deleted a slash character because Django said so. Minor updates

--- DivarClone/users/urls.py
+++ DivarClone/users/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from .views import OTPView
 
 urlpatterns = [
-    path('/otp' , OTPView.as_view(), name='otp_view')
+    path('otp' , OTPView.as_view(), name='otp_view')
 ]
