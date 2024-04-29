34905802b852e19defc2d22f0d3fed5ab0d7167b
Mojo <moalef@live.com>
2024-04-29 11:29:05 +0330

    Updated Admin panel to show Ad class

--- DivarClone/ads/admin.py
+++ DivarClone/ads/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Ad
 
 # Register your models here.
+@admin.register(Ad)
+class AdAdmin(admin.ModelAdmin):
+    list_display = ['title' , 'owner' , 'city' , 'description', 
+                    'category', 'contact' , 'publish' , 'status']
+    list_filter = ['owner' , 'city', 'category', 'status']
+    search_fields = ['title' , 'body']
+    #raw_id_fields = ['owner']
+    date_hierarchy = 'publish'
+    ordering = ['status' , 'publish']
